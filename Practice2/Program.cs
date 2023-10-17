eff451e2604225163c0279963d7ee67e314a76e7
Kronan1 <simonlindberg1992@gmail.com>
2023-10-17 14:04:29 +0200

    Add project files.

---
+++ Practice2/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Practice2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            char[,] matris = new char[25,25];
+
+            // Skriv ut i Consolen så att det ser ut som i Pattern1. Hårdkoda inte använd loopar och if satser eller ternary
+
+
+
+
+            // Samma sak med Pattern2
+
+            
+
+            // Samam sak med pattern3
+
+
+        }
+    }
+}
